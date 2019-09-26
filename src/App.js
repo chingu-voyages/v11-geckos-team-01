d69f0358623e76ed5896c9ba7d325bc409059b13
nickeyvee <nvorraso2020@gmail.com>
2019-09-26 19:26:06 -0400

    resizable box dynamic height and width

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 
-import { ResizableBox } from 'react-resizable'
+// import Resizable from 'react-resizable'
 import './Resizable.css'
 
 import Editor from './Editor'
@@ -9,32 +9,74 @@ import Preview from './Preview'
 import Header from './Header'
 import Footer from './Footer'
 
-function App() {
-  const [content, setContent] = useState('')
+const ResizableBox = require('react-resizable').ResizableBox
+// const Resizable = require('react-resizable').Resizable
 
-  const onChange = (nextState) => {
-    console.log(nextState)
-    setContent(nextState)
+class App extends React.Component {
+  // const [content, setContent] = useState('')
+  constructor(props) {
+    super(props)
+    this.state = {
+      clientWidth: 0,
+      height: 0,
+      content: '',
+      boxHeight: 0,
+      boxWidth: 0
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
   }
-  return (
-    <div className="app">
-      <Header />
-      <div className="flex-container">
-        <ResizableBox className="box flex-item" 
-                      width={500} 
-                      axis="x"
-                      // height={Infinity}
-                      // handleSize={[10, 10]}
-                      >
-          <Editor onChange={onChange} />
-        </ResizableBox>
-        <div className="flex-item">
-          <Preview content={content} />
+  
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+
+    const boxHeight = this.resizableBox.clientHeight
+    const boxWidth = this.resizableBox.clientWidth
+    console.log('Box Height', this.resizableBox.clientHeight)
+    console.log('Window', window.innerHeight)
+    this.setState({ boxWidth, boxHeight })
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+  
+  updateWindowDimensions() {
+    this.setState({ clientWidth: window.innerWidth, height: window.innerHeight })
+  }
+
+  onChange (nextState) {
+    this.setState({ content: nextState })
+  }
+  render () {
+    return (
+      <div className="app">
+        <Header />
+        <div className="flex-container"
+          ref={(resizableBox) => this.resizableBox = resizableBox}
+        >
+          <ResizableBox
+            className="box flex-item"
+            width={this.state.clientWidth / 2}
+            height={this.state.boxHeight}
+            axis="x"
+          >
+            <Preview content={this.content} />
+
+          </ResizableBox>
+          <div className="flex-item"
+            style={{ width: this.state.clientWidth - this.state.boxWidth }}
+          >
+            <Editor
+              onChange={() => {}}
+            />
+          </div>
+          {/* {this.state.boxHeight} */}
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  )
+    )
+  }
 }
 
 export default App
