15ea44d0f1f5199315b83d63c1b296d6183493f4
pablowbk <pablo.weisbek@gmail.com>
2019-09-26 10:03:20 -0300

    Remove width:50% from .flex-item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
 
-// import ResizableBox from 'react-resizable'
+import { ResizableBox } from 'react-resizable'
 import './Resizable.css'
 
 import Editor from './Editor'
 import Preview from './Preview'
 import Header from './Header'
 import Footer from './Footer'
-
-const ResizableBox = require('react-resizable').ResizableBox;
 
 function App() {
   const [content, setContent] = useState('')
@@ -22,7 +20,12 @@ function App() {
     <div className="app">
       <Header />
       <div className="flex-container">
-        <ResizableBox className="box flex-item" width={500} axis="x">
+        <ResizableBox className="box flex-item" 
+                      width={500} 
+                      axis="x"
+                      // height={Infinity}
+                      // handleSize={[10, 10]}
+                      >
           <Editor onChange={onChange} />
         </ResizableBox>
         <div className="flex-item">
