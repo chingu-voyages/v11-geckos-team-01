48ff81c5226607279c79ec7c397c42f6a6642da8
Joseph Morse <tamed.lionheart@gmail.com>
2019-10-03 20:37:29 -0400

    fix(login): Fixed the login with changes to backend
    Some of the changes I made to backend including changing the auth
    login, logout and such routes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
-import uuid from 'uuid/v4'
-import Mustache from 'mustache'
-import axios from 'axios'
+import uuid from 'uuid/v4';
+import Mustache from 'mustache';
+import axios from 'axios';
 
-import Editor from './Editor'
-import Preview from './Preview'
-import Header from './Header'
-import Footer from './Footer'
+import Editor from './Editor';
+import Preview from './Preview';
+import Header from './Header';
+import Footer from './Footer';
 
-const initialValue = `[{\n  'repeat(5, 15)': {\n    accountId: '{{guid}}',\n    notes: [ { 'repeat(5, 10)': { text: null } } ],\n    picture: 'http://placehold.it/32x32',\n    balance: '{{floating(1000, 4000, 2, "$0,0.00")}}'\n  }\n}]`
+const initialValue = `[{\n  'repeat(5, 15)': {\n    accountId: '{{guid}}',\n    notes: [ { 'repeat(5, 10)': { text: null } } ],\n    picture: 'http://placehold.it/32x32',\n    balance: '{{floating(1000, 4000, 2, "$0,0.00")}}'\n  }\n}]`;
 
 // import { formatJSONfromString } from './Utils'
 
 class App extends React.Component {
   // const [value, setValue] = useState('')
   // const [result, setResult] = useState('')
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       user: null,
       docValue: '',
@@ -27,161 +27,157 @@ class App extends React.Component {
       templateId: '',
       loaded: true,
       result: ''
-    }
-    this.findNodes = this.findNodes.bind(this)
-    this.generateJSON = this.generateJSON.bind(this)
-    this.repeatNode = this.repeatNode.bind(this)
-    this.onChange = this.onChange.bind(this)
+    };
+    this.findNodes = this.findNodes.bind(this);
+    this.generateJSON = this.generateJSON.bind(this);
+    this.repeatNode = this.repeatNode.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
-  componentDidMount () {
-    axios.get('/profile', { headers: {
-      'Access-Control-Allow-Origin': '*'
-    }}).then(({ data }) => {
-      console.log(data)
-      if (data.user && data.user.id) {
-        console.log('loggged in')
-        console.log(data)
-        localStorage.setItem('user', JSON.stringify(data))
-        this.setState({ user: data })
-      } else {
-        console.log('logged out')
-      }
-    })
+  componentDidMount() {
+    axios
+      .get('/auth/current_user', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }
+      })
+      .then(({ data }) => {
+        if (data) {
+          localStorage.setItem('user', JSON.stringify(data));
+          this.setState({ user: data });
+        }
+      });
   }
 
-  repeatNode (callback, node, mode) {
-    const args = callback.match(/\d+/g)
-    const parsed = args.map((str) => parseInt(str, 10))
+  repeatNode(callback, node, mode) {
+    const args = callback.match(/\d+/g);
+    const parsed = args.map((str) => parseInt(str, 10));
     // console.log(parsed)
 
-    const max = parsed[1]
-    const min = parsed[0]
-    
-    let i = Math.floor((Math.random() * ((max + 1) - min)) + min)
+    const max = parsed[1];
+    const min = parsed[0];
+
+    let i = Math.floor(Math.random() * (max + 1 - min) + min);
     // console.log(`Repeat this object ${i} times.`)
-    
-    const result = []
-    while(--i) {
-      result.push({ ...node })
+
+    const result = [];
+    while (--i) {
+      result.push({ ...node });
     }
-    return mode === 'json' ? JSON.stringify(result) : result
+    return mode === 'json' ? JSON.stringify(result) : result;
   }
-  findNodes () {
+  findNodes() {
     // https://stackoverflow.com/questions/48612674/depth-first-traversal-with-javascript
-    const schema = []
+    const schema = [];
 
     const callback = ({ node, callback, name = '_root' }) => {
       if (schema.length) {
-        schema[schema.length - 1].node[name] = `{{&${name}}}`
+        schema[schema.length - 1].node[name] = `{{&${name}}}`;
       }
-      schema.push({ node, callback, name })
+      schema.push({ node, callback, name });
       // console.log(schema)
-    }
-    (function recurse (context, name) {
-      console.log(context, name)
-      let prop
+    };
+    (function recurse(context, name) {
+      console.log(context, name);
+      let prop;
       // debugger
-      const regex = /repeat\((\w|\d|\s|,)+\)/g
+      const regex = /repeat\((\w|\d|\s|,)+\)/g;
       // debugger
-      const repeats = context[0] && Object.keys(context[0])[0].match(regex)
+      const repeats = context[0] && Object.keys(context[0])[0].match(regex);
       if (context[0] && repeats) {
         // debugger
-        prop = repeats[0]
-        callback({ node: context[0][prop], callback: repeats[0], name })
-        return recurse(context[0][prop])
+        prop = repeats[0];
+        callback({ node: context[0][prop], callback: repeats[0], name });
+        return recurse(context[0][prop]);
       } else {
         // debugger
         for (let prop in context) {
           if (Array.isArray(context[prop])) {
             // callback({ name: prop, node: context[prop] })
-            return recurse(context[prop], prop)
+            return recurse(context[prop], prop);
           }
         }
       }
-    })(this.state.template)
-    console.log(this.state.template)
-    return schema
+    })(this.state.template);
+    console.log(this.state.template);
+    return schema;
   }
-  async generateJSON () {
-    console.log('CLICK THAT BITCH')
-    let result = ''
-    let lastNode = {}
+  async generateJSON() {
+    console.log('CLICK THAT BITCH');
+    let result = '';
+    let lastNode = {};
     const config = {
       // guid: () => (Math.random() * 999999).toFixed(2)
       guid: () => uuid()
-    }
-    const arr = this.findNodes()
-    debugger
-    console.log(arr)
+    };
+    const arr = this.findNodes();
+    debugger;
+    console.log(arr);
     arr.map(({ node, callback, name }, i) => {
-      lastNode = { node, callback, name }
-      let currentNode = null
+      lastNode = { node, callback, name };
+      let currentNode = null;
       // let nextNode = arr[i + 1]
       // console.log(lastNode)
       // debugger
       if (arr[i + 1]) {
-        currentNode = arr[i + 1].name
+        currentNode = arr[i + 1].name;
       }
-      const newNode = { ...node,
-        [currentNode]: `{{&${currentNode}}}`,
-      }
-      const template = i === 0
-        ? JSON.stringify(this.repeatNode(callback, newNode))
-        : result
+      const newNode = { ...node, [currentNode]: `{{&${currentNode}}}` };
+      const template =
+        i === 0 ? JSON.stringify(this.repeatNode(callback, newNode)) : result;
       if (currentNode) {
-        config[currentNode] = () => `{{&${currentNode}}}`
+        config[currentNode] = () => `{{&${currentNode}}}`;
       }
       if (lastNode.name) {
         config[lastNode.name] = () => {
           // console.log(repeatNode(lastNode.callback, lastNode.node, 'json'))
-          return this.repeatNode(lastNode.callback, lastNode.node, 'json')
-        }
+          return this.repeatNode(lastNode.callback, lastNode.node, 'json');
+        };
       }
       // console.log(config)
-      const string = Mustache.render(template, config)
-      result = string
-        .replace(/("\[)/g, "[")
-        .replace(/(\]")/g, "]")
-      console.log(JSON.parse(result))
+      const string = Mustache.render(template, config);
+      result = string.replace(/("\[)/g, '[').replace(/(\]")/g, ']');
+      console.log(JSON.parse(result));
       // console.log(currentNode)
       // console.log('----------------------END--------------------------------')
       // console.log(result)
-    })
-    console.log(result)
+    });
+    console.log(result);
     // console.log(JSON.parse(result))
 
-    const url = `/update/template`
+    const url = `/update/template`;
     const data = {
       json: result,
       template: this.state.docValue
-    }
-    console.log(url)
-    this.setState({ result, loaded: false })
-    await axios.post(url, data).then((data) => {
-      console.log('TEMPLATE_UPDATED')
-      console.log(data)
-    }).finally(() => {
-      this.setState({ loaded: true })
-    })
+    };
+    console.log(url);
+    this.setState({ result, loaded: false });
+    await axios
+      .post(url, data)
+      .then((data) => {
+        console.log('TEMPLATE_UPDATED');
+        console.log(data);
+      })
+      .finally(() => {
+        this.setState({ loaded: true });
+      });
   }
-  onChange (object, docValue) {
-    console.log(object)
-    this.setState({ template: object, docValue })
-    console.log(this.state.template)
+  onChange(object, docValue) {
+    console.log(object);
+    this.setState({ template: object, docValue });
+    console.log(this.state.template);
     // try {
 
     // } catch (error) {
     //   return false
     // }
   }
-  render () {
+  render() {
     return (
       <Router>
         <div className="app">
-          <Header
-            user={this.state.user}
-            callback={this.generateJSON}        
-          />
+          <Header user={this.state.user} callback={this.generateJSON} />
           <div className="editor-wrapper">
             <div className="flex-container">
               <div className="flex-item">
@@ -193,17 +189,15 @@ class App extends React.Component {
                 />
               </div>
               <div className="flex-item">
-                <Preview
-                  defaultValue={this.state.result}
-                />
+                <Preview defaultValue={this.state.result} />
               </div>
             </div>
           </div>
           <Footer />
-        </div>        
+        </div>
       </Router>
-    )    
+    );
   }
 }
 
-export default App
+export default App;
