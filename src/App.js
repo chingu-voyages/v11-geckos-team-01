b05c2d6f74b2ab0da3d60cb8438a63d0fa22560a
nickeyvee <nvorraso2020@gmail.com>
2019-10-06 19:32:33 -0400

    redesign of json-generating algo

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
     this.generateJSON = this.generateJSON.bind(this)
     this.repeatNode = this.repeatNode.bind(this)
     this.onChange = this.onChange.bind(this)
-    this.bfs_findNodes = this.bfs_findNodes.bind(this)
+    this.findNodes = this.findNodes.bind(this)
   }
   componentDidMount () {
     axios.get('/profile', { headers: {
@@ -68,8 +68,7 @@ class App extends React.Component {
     }
     return mode === 'json' ? JSON.stringify(result) : result
   }
-  bfs_findNodes () {
-    // console.log(this.state.value)
+  findNodes () {
     const queue = [...this.state.value]
     const schema = []
     const explored = []
@@ -77,107 +76,66 @@ class App extends React.Component {
       const nextState = cloneDeep(node)
       for (let prop in node) {
         if (Array.isArray(node[prop])) {
-          console.log(`this prop should be repeated: ${prop}`)
+          // console.log(`this prop should be repeated: ${prop}`)
           nextState[prop] = `{{&${prop}}}`
         }
       }
       return schema.push({ node: nextState, callback, name })
     }
-    // const logger = (node) => {
-    //   schema.push(node)
-    //   return console.log(node)
-    // }
-    function BFS (callback, root) {
-      // var queue=[this];
-      let node = [root]
+    function BFS (callback, rootNode) {
+      let node = [rootNode]
       let name
       while(queue.length > 0) {
         node = queue.shift()
         name = explored.shift()
         let prop
-        // debugger
         if (node && repeats(node)) {
           prop = repeats(node)
           callback({ node: node[prop], callback: repeats(node)[0], name })
           explored.push(prop)
           queue.push(node[prop])
-          // console.log(queue)
         } else {
-          // debugger
           for (let prop in node) {
-            // debugger
             if (Array.isArray(node[prop])) {
-              // queue.push(node[prop], prop, node)
-              // console.log(prop)
-              // console.log(node[prop][0])
               explored.push(prop)
               queue.push(node[prop][0])
             }
           }
-          console.log(queue)
         }
       } 
     }
     BFS(callback, this.state.value)
-    console.log(schema)
     return schema
   }
   generateJSON () {
     let result = ''
     let lastNode = {}
+    const nodes = this.findNodes()
+    const callbacks = nodes.reduce((acc, { node, callback, name }) => ({
+      ...acc, [name]: () => this.repeatNode(callback, node, 'json')
+    }), {})
     const config = {
-      // guid: () => (Math.random() * 999999).toFixed(2)
-      guid: () => uuid()
+      guid: () => uuid(),
+      ...callbacks
     }
-    const arr = this.bfs_findNodes()
-    // console.log(arr)
-    // debugger
-    arr.map(({ node, callback, name }, i) => {
-      lastNode = { node, callback, name }
-      let currentNode = null
-      // let nextNode = arr[i + 1]
-      // console.log(lastNode)
-      debugger
-      if (arr[i + 1]) {
-        currentNode = arr[i + 1].name
-      }
-      const newNode = { ...node,
-        [currentNode]: `{{&${currentNode}}}`,
-      }
-      const template = i === 0
-        ? JSON.stringify(this.repeatNode(callback, newNode))
+    while (nodes.length) {
+      lastNode = nodes.shift()
+      const { callback, node, name } = lastNode
+      let template = ''
+      template = name === '_root'
+        ? JSON.stringify(this.repeatNode(callback, node))
         : result
-      if (currentNode) {
-        config[currentNode] = () => `{{&${currentNode}}}`
-      }
-      if (lastNode.name) {
-        config[lastNode.name] = () => {
-          // console.log(repeatNode(lastNode.callback, lastNode.node, 'json'))
-          return this.repeatNode(lastNode.callback, lastNode.node, 'json')
-        }
-      }
-      // console.log(config)
       const string = Mustache.render(template, config)
       result = string
         .replace(/("\[)/g, "[")
         .replace(/(\]")/g, "]")
-      // console.log(JSON.parse(result))
-      // console.log(currentNode)
-      // console.log('----------------------END--------------------------------')
-      // console.log(result)
-    })
-    // console.log(JSON.parse(result))
+    }
     this.setState({ result })
   }
   onChange (nextState) {
     console.log(nextState)
     this.setState({ value: nextState })
     console.log(this.state.value)
-    // try {
-
-    // } catch (error) {
-    //   return false
-    // }
   }
   render () {
     return (
